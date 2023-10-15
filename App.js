fe9dd8b33584d22a0a4aa7fc1c0c59372709f7fe
hung90909 <114125660+hung90909@users.noreply.github.com>
2023-10-15 15:54:32 +0700

    first_commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const App =() =>{
+  return(
+    <View>
+      <Text>Hello word</Text>
+    </View>
+  )
+}
+
+export default App;
