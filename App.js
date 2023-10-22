3eae37fb6513e3dc00d8e8dd5f87fffce90c4cfb
Phạm Tiến Dũng <phamtiendungvn2363@gmail.com>
2023-10-23 02:51:12 +0700

    update fix

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
-
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Welcome from './Screen/Welcome/welcome';
-import RegisterInformation from './Screen/RegisterInformation';
 import HomeScreen from './Screen/HomeScreen';
 import CartScreen from './Screen/CartScreen';
 import OrderScreen from './Screen/OrderScreen';
 import ProfileScreen from './Screen/ProfileScreen';
-import Ionicons from 'react-native-vector-icons/Ionicons'; // Import the icons you want to use
+import Welcome from './Screen/Welcome/welcome';
+import Welcome1 from './Screen/Welcome/welcome1';
+import Login from './Screen/Login/login';
+import register from './Screen/Login/register';
+import login2 from './Screen/Login/login2';
+import RegisterInformation from './Screen/RegisterInformation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -56,15 +59,6 @@ const BotBottomTabNavigator = () => {
     </Tab.Navigator>
   );
 };
-import { Text, View } from "react-native";
-import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Welcome from "./Screen/Welcome/welcome";
-import Welcome1 from "./Screen/Welcome/welcome1";
-import Register from "./Screen/Login/register";
-import Login from "./Screen/Login/login";
-import Login2 from "./Screen/Login/login2";
 
 const Stack = createNativeStackNavigator();
 
@@ -72,7 +66,31 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Welcome1"
+          component={Welcome1}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Register"
+          component={register}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Login2"
+          component={login2}
+          options={{headerShown: false}}
+        />
         <Stack.Screen
           name="RegisterInformation"
           component={RegisterInformation}
@@ -83,20 +101,7 @@ const App = () => {
           component={BotBottomTabNavigator}
           options={{headerShown: false}}
         />
-
-        <Stack.Screen
-          name="Welcome"
-          component={Welcome}
-          options={{headerShown: false}}
-        />
       </Stack.Navigator>
-        <Stack.Screen name="Welcome" component={Welcome} options={{headerShown:false}}/>
-        <Stack.Screen name="Welcome1" component={Welcome1} options={{headerShown:false}}/>
-        <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
-        <Stack.Screen name="Register" component={Register} options={{headerShown:false}}/>
-        <Stack.Screen name="Login2" component={Login2} options={{headerShown:false}}/>
-        </Stack.Navigator>
-
     </NavigationContainer>
   );
 };
