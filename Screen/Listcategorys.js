0b5c49531fc7b9a17755205701506e4cf7f1daf1
Phạm Tiến Dũng <phamtiendungvn2363@gmail.com>
2023-11-19 20:50:44 +0700

    update fix

--- Screen/Listcategorys.js
+++ Screen/Listcategorys.js
@@ -1,9 +1,37 @@
 import {StyleSheet, Text, View, Pressable, Image, FlatList} from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {danhmucsp} from './data';
 import {useNavigation} from '@react-navigation/native';
 const Listcategorys = () => {
   const [selectedItem, setSelectedItem] = useState(null);
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Tạo đối tượng headers để chứa các thông tin header
+        const headers = {
+          'x-xclient-id': '654c8a081f10540692bdc998', // Thay 'your-client-id' bằng giá trị thực tế
+          Authorization:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTRjOGEwODFmMTA1NDA2OTJiZGM5OTgiLCJlbWFpbCI6ImR1YzEyM0BnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRWR1l3dWY4Z0czSnVvR0FSM1hDSXd1UC9iR0lYSzdGbGJRU1RvNXVFZGdYS1ZWUTNpQlVJYSIsImlhdCI6MTcwMDMxNzU2OSwiZXhwIjoxNzAxMTgxNTY5fQ.TO9PdoaNWPGr6N_o5Ves3T3si0vVeoJZXd_DMf1A50A', // Thay 'your-access-token' bằng giá trị thực tế
+        };
+
+        // Thực hiện GET request đến API endpoint với headers
+        const response = await axios.get(
+          'http://192.168.0.103:3000/v1/api/category/getAllCategory',
+          {
+            headers,
+          },
+        );
+
+        setData(response.data.message.getAllCategory);
+        // console.log(response.data.message.getAllCategory);
+      } catch (error) {
+        console.error(error.response.data);
+      }
+    };
+
+    fetchData();
+  }, []);
   const navigation = useNavigation();
   const renderItem = ({item}) => {
     const isSelected = selectedItem && selectedItem.id === item.id;
@@ -21,9 +49,13 @@ const Listcategorys = () => {
             danhmucspId: item.id,
           });
         }}>
-        <Image source={item.image} />
+        <Image
+          source={{
+            uri: `http://192.168.0.103:3000/uploads/${item?.category_thumb}`,
+          }}
+        />
         <Text style={{marginTop: 10, fontWeight: 'bold', color: 'black'}}>
-          {item.name}
+          {item.category_name}
         </Text>
       </Pressable>
     );
@@ -33,7 +65,7 @@ const Listcategorys = () => {
       <FlatList
         scrollEnabled={false}
         data={danhmucsp}
-        keyExtractor={item => item.id.toString()}
+        keyExtractor={item => item._id}
         numColumns={4} // Set the number of columns to 2
         renderItem={renderItem}
         style={{marginHorizontal: 25, marginVertical: 20}}
--- Screen/Listproducts.js
+++ Screen/Listproducts.js
@@ -1,10 +1,40 @@
 import {StyleSheet, Text, View, Pressable, Image, FlatList} from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {sanpham} from './data';
-
+import axios from 'axios';
 const Listproducts = () => {
   const [selectedItem, setSelectedItem] = useState(null);
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Tạo đối tượng headers để chứa các thông tin header
+        const headers = {
+          'x-xclient-id': '654c8a081f10540692bdc998', // Thay 'your-client-id' bằng giá trị thực tế
+          Authorization:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTRjOGEwODFmMTA1NDA2OTJiZGM5OTgiLCJlbWFpbCI6ImR1YzEyM0BnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRWR1l3dWY4Z0czSnVvR0FSM1hDSXd1UC9iR0lYSzdGbGJRU1RvNXVFZGdYS1ZWUTNpQlVJYSIsImlhdCI6MTcwMDMxNzU2OSwiZXhwIjoxNzAxMTgxNTY5fQ.TO9PdoaNWPGr6N_o5Ves3T3si0vVeoJZXd_DMf1A50A', // Thay 'your-access-token' bằng giá trị thực tế
+        };
+
+        // Thực hiện GET request đến API endpoint với headers
+        const response = await axios.get(
+          'http://192.168.0.103:3000/v1/api/product/getAllProductByUser',
+          {
+            headers,
+          },
+        );
+
+        setData(response.data.message.allProduct);
+        // console.log(response.data.message.allProduct);
+      } catch (error) {
+        console.error(error.response.data);
+      }
+    };
+
+    fetchData();
+  }, []);
   const renderSanpham = ({item}) => {
+    console.log(item);
+
     const isSelected = selectedItem && selectedItem.id === item.id;
     const formatSoldSP = value => {
       if (value >= 1000000) {
@@ -34,11 +64,13 @@ const Listproducts = () => {
           style={{
             height: 200,
           }}
-          source={{uri: item.imageSP}}
+          source={{
+            uri: `http://192.168.0.103:3000/uploads/${item?.product_thumb[0]}`,
+          }}
           resizeMode="contain"
         />
         <Text style={{color: '#1B2028', fontSize: 14}} numberOfLines={2}>
-          {item.nameSP}
+          {item.product_name}
         </Text>
         <View
           style={{
@@ -48,11 +80,9 @@ const Listproducts = () => {
             alignItems: 'center',
           }}>
           <Text style={{color: '#FC6D26', fontSize: 14}}>
-            {formatPrice(item.priceSP)}
+            {formatPrice(item.product_price)}
           </Text>
-          <Text style={{color: '#1B2028', fontSize: 10}}>
-            Đã bán {formatSoldSP(item.soldSP)}
-          </Text>
+          <Text style={{color: '#1B2028', fontSize: 10}}>Đã bán</Text>
         </View>
       </Pressable>
     );
@@ -61,8 +91,8 @@ const Listproducts = () => {
     <View>
       <FlatList
         scrollEnabled={false}
-        data={sanpham}
-        keyExtractor={item => item.id}
+        data={data}
+        keyExtractor={item => item._id}
         renderItem={renderSanpham}
         numColumns={2}
         style={{marginBottom: 10}}
