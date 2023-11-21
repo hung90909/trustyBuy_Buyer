96dfc404a74a4d92e88485260be6f81f7594d1b2
unknown <thonglaanh1011@gmail.com>
2023-11-21 15:13:20 +0700

    feature : Address Screen UI

--- App.js
+++ App.js
@@ -24,6 +24,8 @@ import ChatScreen from './Screen/Chat';
 import {useIsFocused} from '@react-navigation/native';
 import CartScreen from './Screen/CartScreen';
 import DetailProducts from './Screen/detailProduct';
+import ChangPassword from './Screen/ChangPassword';
+import AdressScreen from './Screen/AddressScreen';
 
 const TabTop = createMaterialTopTabNavigator();
 const TabOrder = () => {
@@ -176,6 +178,16 @@ const App = () => {
         <Stack.Screen
           name="DetailProducts"
           component={DetailProducts}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="ChangePassword"
+          component={ChangPassword}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="AdressScreen"
+          component={AdressScreen}
           options={{headerShown: false}}
         />
       </Stack.Navigator>
---
+++ Screen/AddressScreen.js
@@ -0,0 +1,97 @@
+import {StyleSheet, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {Image, Text} from '@rneui/themed';
+import {TouchableOpacity} from 'react-native';
+
+const AdressScreen = () => {
+  const [listAddress, setListAddress] = useState([]);
+  const nav = useNavigation();
+
+  useEffect(() => {
+    getAddress();
+  }, []);
+
+  const getAddress = async () => {
+    const dummyData = [
+      {
+        address: 'Nhà',
+        addressDetail: 'Ngõ 52, Vân Canh, Hoài Đức',
+      },
+      {
+        address: 'Văn phòng',
+        addressDetail: 'Khối 10, Quán Hành,Nghi Lộc',
+      },
+    ];
+    setListAddress(dummyData);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={() => {
+            nav.goBack();
+          }}>
+          <Image
+            style={{
+              width: 25,
+              height: 25,
+            }}
+            source={require('../Resource/icon/back.png')}
+          />
+        </TouchableOpacity>
+        <Text style={{fontSize: 22, marginLeft: 20, fontWeight: 'bold'}}>
+          Địa chỉ
+        </Text>
+      </View>
+      {listAddress.map((address, index) => (
+        <View style={styles.item}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Image
+              style={{
+                width: 55,
+                height: 55,
+                marginRight: 15,
+              }}
+              source={require('../Resource/icon/gps.png')}></Image>
+            <View>
+              <Text>{address.address}</Text>
+              <Text>{address.addressDetail}</Text>
+            </View>
+          </View>
+          <Image
+            style={{
+              width: 25,
+              height: 25,
+            }}
+            source={require('../Resource/icon/edit.png')}></Image>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+export default AdressScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 15,
+    marginTop: 25,
+  },
+  header: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 5,
+    marginVertical: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    backgroundColor: '#e0e0e0',
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+  },
+});
